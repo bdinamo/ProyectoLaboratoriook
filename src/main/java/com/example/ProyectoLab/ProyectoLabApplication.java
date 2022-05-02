40bb19622f568ad90bdf3d15540219e958b03b08
bdinamo <briandinamo@hotmail.com>
2022-05-02 18:58:36 -0300

    1er commitd

---
+++ src/main/java/com/example/ProyectoLab/ProyectoLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoLab/ProyectoLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
